5b579bd5df543f342bd233a02b4f52abbbfd59b7
adryjannn <adrian.bugaj@microsoft.wsei.edu.pl>
2022-03-05 20:07:31 +0100

    Testowy commit

---
+++ 05032022/05032022/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _05032022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
