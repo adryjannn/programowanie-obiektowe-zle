3de4abd91d705020694435f7a6a92589cfa7d385
adryjannn <adrian.bugaj@microsoft.wsei.edu.pl>
2022-03-05 20:18:10 +0100

    nowe zmiany

--- 05032022/05032022/Program.cs
+++ 05032022/05032022/Program.cs
@@ -7,6 +7,18 @@ namespace _05032022
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("TEST");
+            Console.WriteLine("TEST");
         }
+
+        
+
+    }
+
+
+    class Ulamek
+    {
+        private int licznik;
+        private int mianownik;
     }
 }
